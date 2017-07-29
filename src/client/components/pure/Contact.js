322b1fb805ab1f3c1625b735c91a2147e408b7a4
megmatty <meghopweb@me.com>
2017-07-28 17:35:32 -0700

    css for contact form

--- src/client/components/pure/Contact.js
+++ src/client/components/pure/Contact.js
@@ -7,16 +7,18 @@ class Contact extends Component {
       <div className="contact-container">
         <h3>Contact Friend</h3>
         <form className="contact-form">
-          <label htmlFor="to">To:</label>
-          <input type="text" id="to" />
-          <br />
-          <label htmlFor="game">Game:</label>
-          <input type="text" id="game" />
-          <br />
-          <label htmlFor="message">Message:</label>
-          <textarea className="box-shadow rounded-border" id="message" defaultValue="Hey there, I would like to play with you.">
-          </textarea>
-          <br />
+          <div className="form-item">
+            <label htmlFor="to">To:</label>
+            <input type="text" id="to" />
+          </div>
+          <div className="form-item">
+            <label htmlFor="game">Game:</label>
+            <input type="text" id="game" />
+          </div>
+          <div className="text-area">
+            <label htmlFor="message">Message:</label>
+            <textarea className="box-shadow rounded-border" id="message" defaultValue="Hey there, I would like to play with you."></textarea>
+            </div>
           <div className="captcha">
             <input type="checkbox" required id="captcha"/>
             <label htmlFor="captcha">I am not a robot</label>
