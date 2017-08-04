fa9eda627aa22b0dd022e61cc144d871334f824f
megmatty <meghopweb@me.com>
2017-08-04 14:14:25 -0700

    deploy

--- src/server/index.js
+++ src/server/index.js
@@ -80,13 +80,13 @@ app.get('*', games.find);
 
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-// var cors = require('cors'); 
-// app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+var cors = require('cors'); 
+app.use(cors());
 
 //Mail
 var nodemailer = require('nodemailer');
