06cfdaac45c6ec9b92842f8f0bacaaea043cdab3
megmatty <meghopweb@me.com>
2017-08-04 13:48:00 -0700

    deploy

--- src/server/index.js
+++ src/server/index.js
@@ -78,22 +78,10 @@ app.put('/user/:id', function(req, res) {
 
 app.get('/api', games.find);
 
-app.use('/api', function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    console.log(res);
-    // Pass to next layer of middleware
-    next();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
